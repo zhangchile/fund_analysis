ddc12866b8263e13ac68c7c949e986149506c7af
zhangchile <zzlchile@gmail.com>
2017-11-27 19:54:58 +0800

    first url request demo

--- python/app.py
+++ python/app.py
@@ -1,2 +1,12 @@
 # this is app entry 
-print('app entry')
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = "http://guba.eastmoney.com/news,of160222,730973591.html"
+
+page = urlopen(url)
+
+soup = BeautifulSoup(page)
+
+print (soup.prettify())
